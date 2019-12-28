1d361b5f351257bcf8bad838a0fc251f3423eb3f
uzairkorai <uzairahmed097@gmail.com>
2019-12-28 13:31:15 +0500

    first commit

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE)
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// Models
+const { User } = require('./models/user');
+
+/// ======= USERS ======== ///
+
+app.post('/api/users/register', (req, res) => {
+    res.status(200);
+})
+
+const port = process.env.PORT || 3002;
+
+app.listen(port,()=> {
+    console.log(`Server Runing at ${port}`)
+})
