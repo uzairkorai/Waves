81cf0189f2606f37c11124a2c01806662e534d7d
uzairkorai <uzairahmed097@gmail.com>
2019-12-30 22:05:47 +0500

    update

--- server/server.js
+++ server/server.js
@@ -18,8 +18,17 @@ const { User } = require('./models/user');
 
 /// ======= USERS ======== ///
 
-app.post('/api/users/register', (req, res) => {
-    res.status(200);
+app.post('/api/users/register', (req,res) => {
+    const user = new User(req.body);
+
+    user.save((err,doc)=>{
+        if(err) return res.json({success:false,err});
+        res.status(200).json({
+            success: true,
+            userdata: doc
+        })
+    })
+
 })
 
 const port = process.env.PORT || 3002;
